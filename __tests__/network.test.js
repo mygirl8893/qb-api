81561d77bd8fd1eb9a7f20135b1709afb28e4255
Dan Octavian <danoctavian91@gmail.com>
2018-06-21 11:47:39 +0700

    Move code for mocking inside the test class itself

--- __tests__/network.test.js
+++ __tests__/network.test.js
@@ -1,8 +1,33 @@
 import chai from 'chai'
 import HttpStatus from 'http-status-codes'
 import request from 'supertest'
+import Web3 from 'web3'
 
-import app from '../app'
+
+const mockWeb3 = {
+  eth: {
+    net: {
+      isListening: async () => null,
+      getId: async () => 1235
+    }
+  }
+}
+
+console.log("magic")
+Web3.mockImplementation(() => {
+  console.log("wooooo")
+  return mockWeb3
+})
+
+
+/* eslint-disable-next-line no-undef */
+jest.genMockFromModule('web3')
+/* eslint-disable-next-line no-undef */
+jest.mock('web3')
+
+
+const app = require('../app')
+
 
 const { expect } = chai
 

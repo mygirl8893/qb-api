555666ec25140588f3c5eae4f45266ae3f34a513
Dan Octavian <danoctavian91@gmail.com>
2018-07-05 11:42:17 +0700

    Decrease block time and remove exporting the API

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ import config from './src/config/config'
 import TestPrivateChain from './__tests__/integration/testPrivateChain'
 import apiTesting from './__tests__/apiTesting'
 
-const getApp = () => {
-
-  /* eslint-disable-next-line global-require */
-  const app = require("./app")
-
-  return app
-}
-
 const loyaltyTokenABI = config.tokenABI
+const { tokenDBABI } = config
 
 export default {
-  getApp,
   loyaltyTokenABI,
+  tokenDBABI,
   TestPrivateChain,
   apiTesting
 }

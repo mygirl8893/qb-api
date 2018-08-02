ac3cfa068325f5eddcbb4e9e7ba4b4f769060b03
Dan Octavian <danoctavian91@gmail.com>
2018-08-02 15:48:14 +0300

    update the swagger docs for this API call

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
   module.exports = {
     "testEnvironment": "node",
-    "testPathIgnorePatterns": [
-      "dist"
-    ],
-    "transform": {
-      "^.+\\.ts?$": "ts-jest"
-    },
     "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$",
     "moduleFileExtensions": [
       "ts",

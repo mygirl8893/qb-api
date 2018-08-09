4f39a59986ae5f9484b7f5be1e23fb56fb2aab99
Dan Octavian <danoctavian91@gmail.com>
2018-08-09 17:06:15 +0300

    export solidity code

--- index.ts
+++ index.ts
@@ -1,13 +1,27 @@
 import config from './src/config/config'
 import TestPrivateChain from './__tests__/integration/testPrivateChain'
 import apiTesting from './__tests__/apiTesting'
+import * as fs from "fs";
+import * as path from "path";
 
 const loyaltyTokenABI = config.tokenABI
 const { tokenDBABI } = config
+
+const loyaltyTokenCode = fs.readFileSync(
+  path.resolve(__dirname, '../contracts/loyaltyToken.sol'),
+  'utf-8'
+)
+
+const tokenDBCode = fs.readFileSync(
+  path.resolve(__dirname, '../contracts/tokenDB.sol'),
+  'utf-8'
+)
 
 export default {
   loyaltyTokenABI,
   tokenDBABI,
   TestPrivateChain,
-  apiTesting
+  apiTesting,
+  loyaltyTokenCode,
+  tokenDBCode
 }

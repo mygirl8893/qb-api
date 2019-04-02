f07be0f3998d713b8243d0a322590678b4c9bd9e
Dan Octavian <danoctavian91@gmail.com>
2019-04-02 15:51:28 +0800

    passing tests

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,11 +8,10 @@ const env = process.env.NODE_ENV || 'development'
 const envtConfig = Config[env]
 const web3Private = new Web3(envtConfig.rpc.private)
 const web3Public = new Web3(envtConfig.rpc.public)
-let oldWeb3Private = null
 let web3ConnectionsAreReady = false
 
-;
-(async () => {
+// @ts-ignore
+;(async () => {
   log.info(`Connecting to private: ${envtConfig.rpc.private} and public ${envtConfig.rpc.public}`)
   await Promise.all([
     web3Private.eth.net.isListening().catch(() => {
@@ -27,24 +26,6 @@ let web3ConnectionsAreReady = false
   })
   envtConfig.chainID = chainID
 
-  log.info(`Old private chain RPC is configured as ${envtConfig.rpc.oldPrivate}`)
-  if (envtConfig.rpc.oldPrivate) {
-    try {
-      log.info(`Attempting connection to ${envtConfig.rpc.oldPrivate}`)
-      oldWeb3Private = new Web3(envtConfig.rpc.oldPrivate)
-      await oldWeb3Private.eth.net.isListening()
-      const oldChainID = await oldWeb3Private.eth.net.getId().catch(() => {
-        throw new Error('Could not fetch private chainID')
-      })
-      envtConfig.oldChainID = oldChainID
-      log.info(`Successful connection to ${envtConfig.rpc.oldPrivate} and chain ID ${oldChainID}`)
-    } catch {
-      log.error(`Failed to connect to old chain at ${envtConfig.rpc.oldPrivate}`)
-    }
-  } else {
-    log.info(`No old chain configured for connection.`)
-  }
-
   web3ConnectionsAreReady = true
 })().catch((e) => {
   log.error(`${e.stack}`)
@@ -57,11 +38,9 @@ export default {
   getOldChainID: () => envtConfig.oldChainID,
   getQBXAddress: () => Config.qbxContract,
   getPrivateWeb3: () => web3Private,
-  getOldPrivateWeb3: () => oldWeb3Private,
   getPublicWeb3: () => web3Public,
   getPublicRPC: () => envtConfig.rpc.public,
   getPrivateRPC: () => envtConfig.rpc.private,
-  getOldPrivateRPC: () => envtConfig.rpc.oldPrivate,
   getTokenABI: () => Config.tokenABI,
   getQBXTokenABI: () => Config.qbxTokenABI,
   getEnv: () => env,
--- src/transactions/controller.ts
+++ src/transactions/controller.ts
@@ -11,6 +11,7 @@ import publicBlockchain from '../lib/publicBlockchain'
 import qbxFeeCalculator from '../lib/qbxFeeCalculator'
 import log from '../logging'
 import validation from '../validation'
+import utils from '../lib/utils'
 
 const web3 = Config.getPrivateWeb3()
 
@@ -62,6 +63,31 @@ const txBelongsTo = (address, tx, decodedTx) => (
   decodedTx.params[0].value.toLowerCase() === address
 )
 
+
+function formatSingleTransaction(transaction) {
+  transaction.dataValues.to = transaction.toAddress
+  delete transaction.dataValues.toAddress
+  transaction.dataValues.from = transaction.fromAddress
+  delete transaction.dataValues.fromAddress
+  transaction.dataValues.contract = transaction.contractAddress
+
+  if (transaction.token) {
+    delete transaction.token.dataValues.id
+    delete transaction.token.dataValues.brandId
+    delete transaction.token.dataValues.hidden
+  }
+  if (transaction.status) {
+    transaction.dataValues.status = Boolean(parseInt(transaction.status))
+  }
+
+  delete transaction.dataValues.contractAddress
+  delete transaction.dataValues.id
+  delete transaction.dataValues.tokenId
+  delete transaction.dataValues.contractFunction
+  delete transaction.dataValues.txType
+  delete transaction.dataValues.chainId
+}
+
 const getTransactionSchema = Joi.object().keys({
   params: Joi.object().keys({
     hash: validation.ethereumHash().required()
@@ -75,11 +101,10 @@ async function getTransaction(req, res) {
   if (storedTx && storedTx.state !== 'pending') {
 
     const oldChainId = Config.getOldChainID()
-    if (oldChainId && storedTx.chainId && storedTx.chainId === oldChainId) {
-      log.info(`Fetching old chain transaction ${req.params.hash}`)
-      const oldChainTx = await getTx(req.params.hash, Config.getOldPrivateWeb3())
-      oldChainTx.state = 'processed'
-      return res.json(oldChainTx)
+    if (oldChainId && storedTx.chainId && `${storedTx.chainId}` === oldChainId) {
+      log.info(`Fetching old chain transaction ${req.params.hash} from the database.`)
+      formatSingleTransaction(storedTx)
+      return res.json(storedTx)
     }
 
     const tx = await getTx(req.params.hash, web3)

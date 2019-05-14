f09d6300bec1f323e5d1300862445157c2b83252
Dan Octavian <danoctavian91@gmail.com>
2019-05-14 21:47:13 +0800

    update desc

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -9,6 +9,8 @@ import validation from '../validation'
 const web3 = Config.getPrivateWeb3()
 const web3Pub = Config.getPublicWeb3()
 
+const QBX_TOKEN_DESC = 'With qiibee, businesses around the world can run their loyalty programs on the blockchain. Exchange loyalty tokens to QBX and enter the crypto world'
+
 async function getBalance(from: string = null, contractAddress: string): Promise<string> {
   const Token = TokenController.loyaltyToken(contractAddress.toLowerCase())
   let balance = '0'
@@ -34,7 +36,7 @@ async function getQBXToken(from: string = null) {
       totalSupply,
       decimals: parseInt(await QiibeeToken.decimals().call(), 10),
       balance: new BigNumber(balance).toString(10),
-      description: 'Loyalty on the blockchain.',
+      description: QBX_TOKEN_DESC,
       website: 'https://www.qiibee.com',
       logoUrl: 'https://s3.eu-central-1.amazonaws.com/tokens.qiibee/qbx/logo.png'
     }
